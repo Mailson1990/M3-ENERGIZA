7fb037b4f0f31085bbd401e7a9f1ef0aa606c51d
Mailson <mailsom.mota@gmail.com>
2026-06-11 15:07:45 -0300

    rede social

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 const form = document.getElementById('formContato');
 
 form.addEventListener('submit', function(e) {
+
   e.preventDefault();
 
-  const nome = document.getElementById('nome').value;
-  const telefone = document.getElementById('telefone').value;
-  const email = document.getElementById('email').value;
-  const mensagem = document.getElementById('mensagem').value;
+  const nome = document.getElementById('nome').value.trim();
 
-  const texto = `Olá, meu nome é ${nome}%0A
-Telefone: ${telefone}%0A
-Email: ${email}%0A
-Serviço: ${mensagem}`;
+  const telefone = document.getElementById('telefone').value.trim();
 
-  const numero = '5511998680448'; 
+  const email = document.getElementById('email').value.trim();
 
-  const url = `https://wa.me/${numero}?text=${texto}`;
+  const mensagem = document.getElementById('mensagem').value.trim();
+
+  const texto =
+`Olá, me chamo ${nome}
+
+📞 Telefone: ${telefone}
+
+📧 E-mail: ${email}
+
+🛠 Serviço:
+${mensagem}`;
+
+  const numero = '5511998680448';
+
+  const url =
+`https://wa.me/${numero}?text=${encodeURIComponent(texto)}`;
 
   window.open(url, '_blank');
+
 });
